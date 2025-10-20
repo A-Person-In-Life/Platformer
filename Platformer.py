1ccc1a001ee8273afa8abae8111c03471ca25eb4
A-Person-In-Life <gavin@weiner-family.org>
2025-10-20 19:36:27 -0400

    commit

--- Platformer.py
+++ Platformer.py
@@ -14,7 +14,8 @@ FALLGRAV = .5
 JUMP_POWER = -8
 WALL_JUMP = 15
 DASH_POWER = 25
-DASH_DURATION = 36
+DASH_DURATION = 100
+DASH_COOLDOWN = 1000
 
 PLATFORM_HEIGHT =int(HEIGHT * 0.05)
 PLATFORM_WIDTH = int(WIDTH * 0.05)
@@ -31,21 +32,22 @@ class Player:
         self.canJump = True
         self.gravityApply = True
         self.dashDir = 0
-        self.dashStart = False
-        self.startTick = 0
+        self.dashStarted = False
         self.endTick = 0
+        self.dashCooldown = 0
+        self.shiftPressed = False
         self.player = pygame.Rect(PLATFORM_WIDTH + 40, PLATFORM_HEIGHT + PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
 
     def handleInput(self):
-        self.gravityApply = True
         keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_d]:
-            self.vx = min(ACCEL+self.vx,MAX_SPEED)
-        elif keys[pygame.K_a]:
-            self.vx = max(self.vx-ACCEL,-MAX_SPEED)
-        else:
-            self.vx *= .8
+        
+        if not self.dashStarted:
+            if keys[pygame.K_d]:
+                self.vx = min(ACCEL+self.vx,MAX_SPEED)
+            elif keys[pygame.K_a]:
+                self.vx = max(self.vx-ACCEL,-MAX_SPEED)
+            else:
+                self.vx *= .8
 
         if keys[pygame.K_SPACE]:
             if self.onGround() and self.canJump:
@@ -64,34 +66,50 @@ class Player:
             if self.onGround():
                 self.canJump = True
         
-        if keys[pygame.K_LSHIFT] and not self.dashStart:
-            if keys[pygame.K_d]:
-                self.dashDir = 1
-                self.dashStart = True
-            if keys[pygame.K_a]:
-                self.dashDir = -1
-                self.dashStart = True
+        if keys[pygame.K_LSHIFT] and not self.dashStarted and self.canDash:
+            current_time = pygame.time.get_ticks()
+            if current_time >= self.dashCooldown:
+                if keys[pygame.K_d]:
+                    self.dashDir = 1
+                    self.dashing()
+                elif keys[pygame.K_a]:
+                    self.dashDir = -1 
+                    self.dashing()
         
+        if not keys[pygame.K_LSHIFT]:
+            self.canDash = True
+                
+        if keys[pygame.K_q]:
+            #will do attack at some point
+            pass
     def onGround(self):
         return (self.player.bottom >= HEIGHT - PLATFORM_HEIGHT)
     
     def dashing(self):
-        if self.dashStart:
-            self.startTick = pygame.time.get_ticks()
-            self.endTick = self.startTick + DASH_DURATION
+        if not self.dashStarted:
+            self.endTick = pygame.time.get_ticks() + DASH_DURATION
+            self.dashCooldown = pygame.time.get_ticks() + DASH_COOLDOWN
             self.dashStarted = True
-            
-        pass
+            self.canDash = False
+        
+        if pygame.time.get_ticks() >= self.endTick:
+            self.gravityApply = True
+            self.dashStarted = False
+        else:
+            self.gravityApply = False
+            self.vx = DASH_POWER * self.dashDir
+            self.vy = 0
 
     def applyGravity(self):
-        if not self.onGround():
-            if self.jumping and self.vy < 0:
-                self.vy += JUMPGRAV * .5
-            else:
-                self.vy += FALLGRAV
-        elif (self.vy > 0):
-            self.vy = 0
-            self.player.bottom = HEIGHT - PLATFORM_HEIGHT
+        if self.gravityApply:
+            if not self.onGround():
+                if self.jumping and self.vy < 0:
+                    self.vy += JUMPGRAV * .5
+                else:
+                    self.vy += FALLGRAV
+            elif (self.vy > 0):
+                self.vy = 0
+                self.player.bottom = HEIGHT - PLATFORM_HEIGHT
     
     def checkCollisions(self, platforms):
         self.onWall = False
@@ -111,8 +129,9 @@ class Player:
                     self.vx = 0
             
     def update(self, platforms):
-        #handles inputs, gravity and movement
         self.handleInput()
+        if self.dashStarted:
+            self.dashing()  
         self.applyGravity()
         self.player.x += self.vx
         self.player.y += self.vy
@@ -132,9 +151,7 @@ class Game:
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         self.clock = pygame.time.Clock()
         self.running = True
-        #Player setup
         self.player = Player()
-        #Platform setup
         self.platforms = [pygame.Rect(0, HEIGHT-PLATFORM_HEIGHT,WIDTH,PLATFORM_HEIGHT), 
                           pygame.Rect(WIDTH - PLATFORM_WIDTH, 0, PLATFORM_WIDTH,HEIGHT), 
                           pygame.Rect(0, 0, PLATFORM_WIDTH, HEIGHT)]
